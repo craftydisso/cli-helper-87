519cfb9cf114760f969adc82f9cc29a58b684449
Ioana Popescu <girlcynthiastatesstarstatestastic02@outlook.com>
2026-08-19 19:14:13 +0300

    feat:refactor logging setup

--- logger.py
+++ logger.py
@@ -1,11 +1,28 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='./app.log', level=logging.INFO, max_bytes=1000000, backup_count=3):
-    logger = logging.getLogger()
-    logger.setLevel(level)
-    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    return logger
+class Logger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def debug(self, msg):
+        self.logger.debug(msg)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def warning(self, msg):
+        self.logger.warning(msg)
+
+    def error(self, msg):
+        self.logger.error(msg)
+
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+logger = Logger(__name__)
